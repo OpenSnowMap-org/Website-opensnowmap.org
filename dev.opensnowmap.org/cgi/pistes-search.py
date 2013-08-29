92777d70f3c92b0e65c41c6c8e71040915e7e8b7
yvecai <yvecai@gmail.com>
2013-08-29 22:59:02 +0200

    return array of json, topo to do

--- dev.opensnowmap.org/cgi/pistes-search.py
+++ dev.opensnowmap.org/cgi/pistes-search.py
@@ -25,7 +25,7 @@ def application(environ,start_response):
 			ids=ids.split('&')[0]
 			response={}
 			response=query_topo(ids)
-			response_body=json.dumps(response)
+			response_body=json.dumps(response, sort_keys=True, indent=4)
 			status = '200 OK'
 			response_headers = [('Content-Type', 'application/json'),('Content-Length', str(len(response_body)))]
 			start_response(status, response_headers)
@@ -36,16 +36,16 @@ def application(environ,start_response):
 		if name.find('&'): name=name.split('&')[0]
 	if request.find('point=') !=-1:
 		point=request.split('point=')[1]
-		if point.find('&'): point=point.split('&')[0]
+		if point.find('&'): point=point.split('&')[0].replace(';',',')
 	if request.find('radius=') !=-1:
 		radius=request.split('radius=')[1]
 		if radius.find('&'): radius=radius.split('&')[0]
 	sites, entrances, routes, ways = query_ids(name,point,radius)
-	response={}
-	response['sites']= query_sites(sites)
-	response['routes']= query_routes(routes, full)
-	response['pistes']= query_ways(ways,full)
-	response['aerialways']= query_aerialways(ways,full)
+	response=[]
+	response.extend(query_sites(sites))
+	response.extend(query_routes(routes, full))
+	response.extend(query_ways(ways,full))
+	response.extend(query_aerialways(ways,full))
 	response_body=json.dumps(response, sort_keys=True, indent=4)
 	status = '200 OK'
 	response_headers = [('Content-Type', 'application/json'),('Content-Length', str(len(response_body)))]
@@ -100,6 +100,22 @@ def query_ids(name='', point='', radius=''):
 			idx=long(i[0])
 			if idx < 0: routes_ids.append(str(idx))
 			else: ways_ids.append(str(idx))
+	elif point != '' :
+		radius = 50
+		cur.execute(" \
+		SELECT osm_id \
+		FROM \
+		  planet_osm_line \
+		ORDER BY \
+		  ST_Distance(way, ST_Transform(ST_SetSRID(ST_MakePoint(%s),4326),900913)) ASC\
+		LIMIT 1;" %(point,))
+		ids=cur.fetchall()
+		for i in ids:
+			idx=long(i[0])
+			if idx < 0: routes_ids.append(str(idx))
+			else: ways_ids.append(str(idx))
+		ids=ids[0]
+	
 	con.close()
 	return sites_ids, entrances_ids, routes_ids, ways_ids
 	
@@ -139,6 +155,7 @@ def query_topo(str_id):
 					from planet_osm_line where osm_id = -%s" % (m))
 					topo[i]['member_of'].append(cur.fetchone())
 			topo[i]['aerialway']=s[5]
+		else : i-=1
 	
 	#remove duplicates
 	clean_topo={}
@@ -160,26 +177,30 @@ def equal(d1,d2):
 def query_sites(sites_ids):
 	con = psycopg2.connect("dbname=pistes-mapnik user=mapnik")
 	cur = con.cursor()
-	sites={}
+	sites=[]
 	for idx in sites_ids:
+		site={}
 		cur.execute("select site_name, \"piste:type\", ST_AsLatLonText(ST_Transform(way,4326), 'D.DDDDD') \
 		from planet_osm_point where osm_id = %s and \"piste:type\" is not null;"\
 		%(idx))
 		resp=cur.fetchall()
 		for s in resp:
 			if s:
-				sites[idx]={}
-				sites[idx]['name']=s[0]
-				sites[idx]['types']=s[1]
-				sites[idx]['center']=s[2].split(' ')[1]+','+s[2].split(' ')[0]
+				site['id']=idx
+				site['type']='site'
+				site['name']=s[0]
+				site['types']=s[1]
+				site['center']=s[2].split(' ')[1]+','+s[2].split(' ')[0]
+		if len(site) != 0: sites.append(site)
 	con.close()
 	return sites
 	
 def query_routes(routes_ids, full):
 	con = psycopg2.connect("dbname=pistes-mapnik user=mapnik")
 	cur = con.cursor()
-	routes={}
+	routes=[]
 	for idx in routes_ids:
+		route={}
 		cur.execute("select \
 				route_name, \
 				\"piste:type\", \
@@ -191,12 +212,13 @@ def query_routes(routes_ids, full):
 		resp=cur.fetchall()
 		for s in resp:
 			if s:
-				routes[idx]={}
-				routes[idx]['name']=s[0]
-				routes[idx]['types']=s[1]
-				routes[idx]['center']=s[2].split(' ')[1]+','+s[2].split(' ')[0]
-				if not s[3]: routes[idx]['color']=s[4]
-				else:  routes[idx]['color']=s[3]
+				route['id']=idx
+				route['type']='route'
+				route['name']=s[0]
+				route['types']=s[1]
+				route['center']=s[2].split(' ')[1]+','+s[2].split(' ')[0]
+				if not s[3]: route['color']=s[4]
+				else:  route['color']=s[3]
 		if full:
 			cur.execute("select in_site \
 				from planet_osm_line where osm_id = %s and \"piste:type\" is not null"\
@@ -204,25 +226,27 @@ def query_routes(routes_ids, full):
 			resp=cur.fetchall()
 			try :
 				# only the first site is taken into account
-				s=-resp[0][0][0]
+				site_id=-resp[0][0][0]
 				cur.execute("select site_name,  \
 					ST_AsLatLonText(st_centroid(ST_Transform(way,4326)),'D.DDDDD') \
 					from planet_osm_point where osm_id = %s and site_name is not null"\
-					%(s))
+					%(site_id))
 				resp2=cur.fetchall()
-				routes[idx]['site_name']=resp2[0][0]
-				routes[idx]['site_center']=resp2[0][1].split(' ')[1]+','+resp2[0][1].split(' ')[0]
+				route['site_id']=-site_id
+				route['site_name']=resp2[0][0]
+				route['site_center']=resp2[0][1].split(' ')[1]+','+resp2[0][1].split(' ')[0]
 			except:
 				pass
-				
+		if len(route) !=0 :routes.append(route)
 	con.close()
 	return routes
 	
 def query_ways(ways_ids,full):
 	con = psycopg2.connect("dbname=pistes-mapnik user=mapnik")
 	cur = con.cursor()
-	ways={}
+	ways=[]
 	for idx in ways_ids:
+		way={}
 		cur.execute("select COALESCE(name,'')||' '||COALESCE(\"piste:name\",''), \"piste:type\", \
 		ST_AsLatLonText(st_centroid(ST_Transform(way,4326)), 'D.DDDDD'), \"piste:difficulty\", \"piste:grooming\", \"piste:lit\" \
 		from planet_osm_line where osm_id = %s and \"piste:type\" is not null;"\
@@ -230,13 +254,14 @@ def query_ways(ways_ids,full):
 		resp=cur.fetchall()
 		for s in resp:
 			if s:
-				ways[idx]={}
-				ways[idx]['name']=s[0]
-				ways[idx]['types']=s[1]
-				ways[idx]['center']=s[2].split(' ')[1]+','+s[2].split(' ')[0]
-				ways[idx]['difficulty']=s[3]
-				ways[idx]['grooming']=s[4]
-				ways[idx]['lit']=s[5]
+				way['id']=idx
+				way['type']='piste'
+				way['name']=s[0]
+				way['types']=s[1]
+				way['center']=s[2].split(' ')[1]+','+s[2].split(' ')[0]
+				way['difficulty']=s[3]
+				way['grooming']=s[4]
+				way['lit']=s[5]
 		if full:
 			cur.execute("select in_site \
 				from planet_osm_line where osm_id = %s and \"piste:type\" is not null"\
@@ -244,33 +269,37 @@ def query_ways(ways_ids,full):
 			resp=cur.fetchall()
 			try :
 				# only the first site is taken into account
-				s=-resp[0][0][0]
+				site_id=-resp[0][0][0]
 				cur.execute("select site_name,  \
 					ST_AsLatLonText(st_centroid(ST_Transform(way,4326)),'D.DDDDD') \
 					from planet_osm_point where osm_id = %s and site_name is not null"\
-					%(s))
+					%(site_id))
 				resp2=cur.fetchall()
-				ways[idx]['site_name']=resp2[0][0]
-				ways[idx]['site_center']=resp2[0][1].split(' ')[1]+','+resp2[0][1].split(' ')[0]
+				way['site_id']=-site_id
+				way['site_name']=resp2[0][0]
+				way['site_center']=resp2[0][1].split(' ')[1]+','+resp2[0][1].split(' ')[0]
 			except:
 				pass
+		if len(way) !=0 : ways.append(way)
 	con.close()
 	return ways
 
 def query_aerialways(ways_ids,full):
 	con = psycopg2.connect("dbname=pistes-mapnik user=mapnik")
 	cur = con.cursor()
-	aerialways={}
+	aerialways=[]
 	for idx in ways_ids:
+		aerialway={}
 		cur.execute("select name, aerialway, ST_AsLatLonText(st_centroid(ST_Transform(way,4326)), 'D.DDDDD') from planet_osm_line where osm_id = %s and aerialway is not null;"\
 		%(idx))
 		resp=cur.fetchall()
 		for s in resp:
 			if s:
-				aerialways[idx]={}
-				aerialways[idx]['name']=s[0]
-				aerialways[idx]['types']=s[1]
-				aerialways[idx]['center']=s[2].split(' ')[1]+','+s[2].split(' ')[0]
+				aerialway['id']=idx
+				aerialway['type']='aerialway'
+				aerialway['name']=s[0]
+				aerialway['types']=s[1]
+				aerialway['center']=s[2].split(' ')[1]+','+s[2].split(' ')[0]
 		if full:
 			cur.execute("select in_site \
 				from planet_osm_line where osm_id = %s and \"piste:type\" is not null"\
@@ -284,10 +313,12 @@ def query_aerialways(ways_ids,full):
 					from planet_osm_point where osm_id = %s and site_name is not null"\
 					%(s))
 				resp2=cur.fetchall()
-				aerialways[idx]['site_name']=resp2[0][0]
-				aerialways[idx]['site_center']=resp2[0][1].split(' ')[1]+','+resp2[0][1].split(' ')[0]
+				aerialway['site_name']=resp2[0][0]
+				aerialway['site_center']=resp2[0][1].split(' ')[1]+','+resp2[0][1].split(' ')[0]
 			except:
 				pass
+		
+		if len(aerialway) !=0 : aerialways.append(aerialway)
 	con.close()
 	return aerialways
 #6.46,46.83
