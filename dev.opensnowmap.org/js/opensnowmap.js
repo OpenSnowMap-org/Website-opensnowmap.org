2358b6a7d4f4558e20dd19ec265bcfdde60ca302
yves <webmaster@opensnowmap.org>
2019-12-15 07:03:46 +0100

    Test new tile server

--- dev.opensnowmap.org/js/opensnowmap.js
+++ dev.opensnowmap.org/js/opensnowmap.js
@@ -33,10 +33,10 @@ if (!window.location.host) {
 }
 if (server.search('home') != -1){ server = protocol+"//beta.opensnowmap.org/";}
 
-var pistes_and_relief_overlay_URL=protocol+"//www.opensnowmap.org/pistes-relief/";
+var pistes_and_relief_overlay_URL=protocol+"//116.202.116.205/pistes-relief/";
 //var pistes_only_overlay_URL=protocol+"//www.opensnowmap.org/pistes/";
-var pistes_only_overlay_URL="https:"+"//127.0.0.1/pistes/";
-var snow_base_layer_URL =protocol+"//www.opensnowmap.org/base_snow_map/";
+var pistes_only_overlay_URL="http:"+"//116.202.116.205/pistes/";
+var snow_base_layer_URL =protocol+"//116.202.116.205/base_snow_map/";
 
 var mode = "raster";
 var EXT_MENU = true;
